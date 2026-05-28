b5a13bfb80b6a68e71aca2fbe2a854cb134dc3c7
Avijit Karmokar <ronykormokar990@gmail.com>
2026-05-28 13:52:49 +0600

    Fix formatting issue in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/avijit/'
+    base: '/avijit/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
